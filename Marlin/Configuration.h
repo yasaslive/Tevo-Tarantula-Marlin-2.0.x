397da4df1853a6d2f256181464f82a63aefa450a
Yasas Alwis <yasaslive@gmail.com>
2021-02-24 19:34:10 +0530

    Fix MAXLENGTH to 320

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -569,7 +569,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 200
+#define EXTRUDE_MAXLENGTH 320
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
