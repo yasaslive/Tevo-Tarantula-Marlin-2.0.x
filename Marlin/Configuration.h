8d21d74c97924ddc4715ee1b2e9ed3e836900bbd
Yasas Alwis <it17181402@my.sliit.lk>
2021-03-23 01:58:16 +0530

    minor adjestments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -738,7 +738,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 408 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
