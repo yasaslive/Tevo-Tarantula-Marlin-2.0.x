d46680d2df7cc589ef1804cd5703a86e1e6e4d39
Yasas Alwis <it17181402@my.sliit.lk>
2021-03-23 12:32:50 +0530

    minor steps/mm calibration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -738,7 +738,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 408.6 }
 
 /**
  * Default Max Feed Rate (mm/s)
